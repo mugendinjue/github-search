35c10ff685e97c144dd326392f14b01a5ddc1061
mugendinjue <mugendinjue7@gmail.com>
2019-08-23 18:28:32 +0300

    Add the http service

--- src/app/services/http-sevice.service.ts
+++ src/app/services/http-sevice.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Users  } from '../users';
+import { environment } from '../../environments/environment';
+// import { resolve } from 'dns';
+// import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpSeviceService {
 
-  constructor() { }
+  user : Users;
+
+  constructor(private http : HttpClient ) { }
+
+  searchGitUser(userN){
+    interface Result{
+      avatar_url:string;
+      login : string;
+      public_repo:string;
+      repos_url:string;
+      followers:string;
+      following:string;
+    }
+    let promise = new Promise ((resolve,reject)=>{
+      this.http.get<Result>('https://api.github.com/users/'+userN+'?access_token='+environment.apiKey).toPromise().then(
+        (result)=>{
+      this.user = new Users(result.login,result.avatar_url,result.public_repo,result.followers,result.following)
+      resolve()
+        },(error)=>{
+          console.log(error);
+          reject(error);
+
+        })
+    })
+    return promise
+  }
+
 }
--- src/app/users.ts
+++ src/app/users.ts
@@ -1,5 +1,5 @@
 export class Users {
-    constructor(public gitHubUserName : string,public repoNumber : number,public dateCreated : Date,public followers : number,public following :number){
+    constructor(public gitHubUserName : string,public avatar : string,public repoName : string,public followers : string,public following :string){
 
     }
 }
