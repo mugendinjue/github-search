b47445e3dfed547ebee7e6ac86748aa96f2c1b97
mugendinjue <mugendinjue7@gmail.com>
2019-08-23 17:14:16 +0300

    Add a user class constructor

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,5 @@
 export class Users {
+    constructor(public gitHubUserName : string,public repoNumber : number,public dateCreated : Date,public followers : number,public following :number){
+
+    }
 }
