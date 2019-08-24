1daafa49d3b4ae916380aadc74a8edcc6463ffa2
mugendinjue <mugendinjue7@gmail.com>
2019-08-24 13:10:34 +0300

    remove comments

--- src/app/services/http-sevice.service.ts
+++ src/app/services/http-sevice.service.ts
@@ -32,12 +32,6 @@ export class HttpSeviceService {
       this.http.get<Result>('https://api.github.com/users/'+userN+'?access_token='+environment.apiKey).toPromise().then(
         (result)=>{
       this.user = new Users(result.login,result.avatar_url,result.followers,result.following,result.created_at,result.public_repos);
-  
-
-      
-      // this.defaultUser.push(this.user);
-      // console.log(this.defaultUser);
-      
       resolve()
         },(error)=>{
           console.log(error);
