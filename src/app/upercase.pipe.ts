dc3f18633157522b14322807a75d53c35326d117
mugendinjue <mugendinjue7@gmail.com>
2019-08-25 14:00:50 +0300

    add a working uppercase up

--- src/app/upercase.pipe.ts
+++ src/app/upercase.pipe.ts
@@ -5,8 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UpercasePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): string {
+    let newStr = value;
+    var upStr = newStr.toUpperCase()
+    return upStr;
   }
 
 }
